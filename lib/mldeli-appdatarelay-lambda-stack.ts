0ff4e008343c85a75d82fc458bacc461c0e21017
ishizuka.naoto <ishizuka.naoto@sapporo.coop>
2023-11-29 15:33:56 +0900

    yamlの更新

--- lib/mldeli-appdatarelay-lambda-stack.ts
+++ lib/mldeli-appdatarelay-lambda-stack.ts
@@ -1,7 +1,42 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, aws_iam as iam } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { AppStackProps } from "types";
 
+export class IamStack extends Stack {
+  constructor(scope: Construct, id: string, props: AppStackProps) {
+    super(scope, id, props);
+
+    // --------------------------------------------------------------
+    // スタックにPermissionsBoundary を適用
+    //
+
+    const permissionsBoundaryPolicy = iam.ManagedPolicy.fromManagedPolicyArn(
+      this,
+      "builders-delegated-iam-boundary",
+      `arn:aws:iam::${props.account}:policy/builders-delegated-iam-boundary`,
+      //permissionsBoundaryArn
+    );
+
+    iam.PermissionsBoundary.of(this).apply(permissionsBoundaryPolicy);
+        
+    
+    // --------------------
+    // create-mldeli-file-lambda
+    //
+    const createmldeliLambdaName = "create-mldeli-file";
+    const createMldeliFileRoleName = `${props.env}-${createMldeliFileRoleName}-role`;
+    new iam.Role(this, createmldeliLambdaName, {
+      roleName: createmldeliLambdaName,
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      permissionsBoundary: permissionsBoundaryPolicy,
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AWSLambdaExecute"),
+        iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
+      ],
+    });
+
+  }
+}
 export class MldeliAppdatarelayLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
